f31b24970feb2b824d5bbc7618b07751b3c7a9aa
Marcus Andersson <marcus@apparant.se>
2023-01-08 21:14:07 +0100

    Grunden till webbbtjänsten + installerat paket

---
+++ app.js
@@ -0,0 +1,29 @@
+/* REST-api i NodeJS + Express
+Av: Marcus Andersson, maan2117@student.miun.se */
+
+//Grundläggande imports och inställningar
+const express = require("express");
+const app = express();
+const port = 3000;
+const cors = require("cors"); //För att slippa CORS-error
+app.use(cors({ origin: '*' }));
+app.use(express.json());
+
+//Importera Routes
+let usersRouter = require("./routes/users");
+let todoListRouter = require("./routes/todoList");
+
+//Ifall användare besöker ./ så möts de av ett statusmeddelande i JSON-format
+app.get("/", (req, res, next) => { 
+    res.json({"Status": "Online!"});
+});
+
+//Knyt URI till router
+app.use("/users", usersRouter);
+app.use("/todoList", todoListRouter);
+
+
+//Starta server och lyssna på port 3000
+app.listen(port, function() {
+    console.log(`Servern startades och lyssnar på port: ${port}`);
+});
