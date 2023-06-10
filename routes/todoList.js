3fe92d1f002f96b3582b598d4f4936d233898ce0
Marcus Andersson <marcus@apparant.se>
2023-06-10 21:28:33 +0200

    DELETE fungerar + 404 vid ej hittade

--- routes/todoList.js
+++ routes/todoList.js
@@ -20,19 +20,37 @@ let Todo = mongoose.model('Todo', todoSchema);
 /* Hämta (GET) todos */
 router.get('/', async function(req, res, next) {
   let todos = await Todo.find({}); //Hitta alla Todos
-  res.send(todos); //Returnerar alla todos
+
+  if(todos !== null) { //Så länge det finns träffar
+    res.send(todos); //Returnerar alla todos
+  }
+  else {
+    res.status(404);
+    res.send("Du har inget att göra...");
+  }
 });
 
 /* Hämta specifikt Todo */
 router.get('/:id', async (req, res, next) => {
   let id = req.params.id; //Läs in det Id som skickats med som parameter
+
   let todo = await Todo.findById(id); //Hitta en specifit "Att göra" via dess Id
-  res.send(todo); //Returnera den funna "att-göra"
+
+  //Om todo inte är null
+  if(todo !== null) {
+    res.send(todo); //Returnerar den hittade "att-göra"
+  }
+  else { //Returenra 404 - not found + felmeddelande
+    res.status(404);
+    res.send(`Kunde inte hitta todo med id: ${id}`)
+  }
+  
 });
 
 /* Uppdatera Todo */
 router.put('/:id', async (req, res, next) => {
-  let id = req.params.id;
+  let id = req.params.id;//Läser in det Id som skickats med som parameter
+
   res.send(`UPPDATERA Specifikt todo med id: ${id}`);
 });
 
@@ -50,13 +68,30 @@ router.post('/', async (req, res, next) => {
   //Spara ny Todo i Databasen
   await todo.save().catch(err => console.log(err)); //Konsol-logga eventuella fel
 
-  res.send(todo);
+  //Returnera meddelande att Todo skapats
+  res.send({
+    "status": "Todo skapad", 
+    "Data som lagrats": todo 
+  });
 });
 
 /* Radera Todo (Delete) */
 router.delete('/:id', async (req, res, next) => {
-  let id = req.params.id;
-  res.send(`RADERA Specifikt todo med id: ${id}`);
+  let id = req.params.id; //Läs in id
+
+  let todo = await Todo.findById(id);
+
+  //Om todo hittades
+  if(todo) {
+    todo.deleteOne(); //Radera
+    //Returnera meddelande
+    res.send({
+      "status": "Lyckades",
+      "text": `Todo med Id ${id} raderades!`
+    });
+  }
+
+
 });
 
 
