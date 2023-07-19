29713813bc716b5307f0a68d9f63b5cd5a0a40e4
Marcus Andersson <marcus@apparant.se>
2023-07-19 12:40:55 +0200

    Fixat bugg vid uppdatering av todoIsDone

--- routes/todoList.js
+++ routes/todoList.js
@@ -1,108 +1,117 @@
-var express = require('express');
-var mongoose = require('mongoose');
+var express = require("express");
+var mongoose = require("mongoose");
 var router = express.Router();
 
 //Koppla upp mot MongoDb med Mongoose
-mongoose.connect('mongodb://127.0.0.1:27017/todoListDb')
-.catch(error => console.log(error)); //Konsol-logga felet ifall anslutning misslyckas
+mongoose
+  .connect("mongodb://127.0.0.1:27017/todoListDb")
+  .catch((error) => console.log(error)); //Konsol-logga felet ifall anslutning misslyckas
 mongoose.Promise = global.Promise;
 
 //Skapa schema
 let todoSchema = mongoose.Schema({
   todoTitle: String,
   todoDescription: String,
-  todoIsDone: Boolean
+  todoIsDone: Number,
 });
 
 //Skapa Model
-let Todo = mongoose.model('Todo', todoSchema);
+let Todo = mongoose.model("Todo", todoSchema);
 
 /* --------------------------------------------------Hämta (GET) todos --------------------------------------------------*/
-router.get('/', async function(req, res, next) {
+router.get("/", async function (req, res, next) {
   let todos = await Todo.find({}); //Hitta alla Todos
 
-  if(todos !== null) { //Så länge det finns träffar
+  if (todos !== null) {
+    //Så länge det finns träffar
     res.send(todos); //Returnerar alla todos
-  }
-  else {
+  } else {
     res.status(404);
     res.send("Du har inget att göra...");
   }
 });
 
 /* --------------------------------------------------Hämta specifikt Todo-------------------------------------------------- */
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   let id = req.params.id; //Läs in det Id som skickats med som parameter
 
   let todo = await Todo.findById(id); //Hitta en specifit "Att göra" via dess Id
 
   //Om todo inte är null
-  if(todo !== null) {
+  if (todo !== null) {
     res.send(todo); //Returnerar den hittade "att-göra"
-  }
-  else { //Returenra 404 - not found + felmeddelande
+  } else {
+    //Returenra 404 - not found + felmeddelande
     res.status(404);
-    res.send(`Kunde inte hitta todo med id: ${id}`)
+    res.send(`Kunde inte hitta todo med id: ${id}`);
   }
-  
 });
 
 /* --------------------------------------------------Skapa nytt Todo (Post)-------------------------------------------------- */
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   const jsonData = req.body; //Läs in den Json-data som skickats med i Body i anropet
 
   //Skapa ny instans av modellen Todo, lägg in datat som skickats med..
   let todo = new Todo({
     todoTitle: jsonData.todoName,
     todoDescription: jsonData.todoDesc,
-    todoIsDone: false
+    todoIsDone: false,
   });
 
   //Spara ny Todo i Databasen
-  await todo.save().catch(err => console.log(err)); //Konsol-logga eventuella fel
+  await todo.save().catch((err) => console.log(err)); //Konsol-logga eventuella fel
 
   //Returnera meddelande att Todo skapats
   res.status(201); //Http 201, created
   res.send({
-    "status": "Todo skapad", 
-    "Data som lagrats": todo 
+    status: "Todo skapad",
+    "Data som lagrats": todo,
   });
 });
 
 /* --------------------------------------------------Uppdatera Todo-------------------------------------------------- */
-router.put('/:id', async (req, res, next) => {
-  let id = req.params.id;//Läser in det Id som skickats med som parameter
+router.put("/:id", async (req, res, next) => {
+  let id = req.params.id; //Läser in det Id som skickats med som parameter
 
   let foundTodo = await Todo.findById(id); //Hitta en specifikt "Att göra" via dess Id
 
-  if(foundTodo === null) {  //Hittades inte
+  if (foundTodo === null) {
+    //Hittades inte
     res.status(404);
     res.send(`Kunde inte hitta todo med id ${id}`);
-  }
-  else { //Hittades
+  } else {
+    //Hittades
     const jsonData = req.body; //Läs in den Json-data som skickats med i Body i anropet
 
     //Skapa ny instans av modellen Todo, lägg in datat som skickats med..
     let todo = new Todo({
       todoTitle: jsonData.todoName,
       todoDescription: jsonData.todoDesc,
-      todoIsDone: false
+      todoIsDone: jsonData.todoIsDone,
     });
-  
+
     //Uppdatera dokumentet
-    let result = await Todo.updateOne({_id: id}, { todoTitle: todo.todoTitle, todoDescription: todo.todoDescription, todoIsDone: todo.todoIsDone }).catch((err) => { console.log(err)});
-  
+    let result = await Todo.updateOne(
+      { _id: id },
+      {
+        todoTitle: todo.todoTitle,
+        todoDescription: todo.todoDescription,
+        todoIsDone: todo.todoIsDone,
+      }
+    ).catch((err) => {
+      console.log(err);
+    });
+
     console.log("RESULTAT: " + JSON.stringify(result));
 
-    if(result.modifiedCount == 1) { //Ett (1) dokument har modifierats
+    if (result.modifiedCount == 1) {
+      //Ett (1) dokument har modifierats
       res.status(200); //Lyckades
       res.send(todo);
-    }
-    else if(result.modifiedCount == 0 && result.matchedCount == 1) {
+    } else if (result.modifiedCount == 0 && result.matchedCount == 1) {
       res.status(400); //Error-kod (Internal server error)
       res.send("Fel: Ingen data att uppdatera");
-    }
-    else {
+    } else {
       res.status(500); //Error-kod (Internal server error)
       res.send("Fel: kunde inte uppdatera");
     }
@@ -110,32 +119,31 @@ router.put('/:id', async (req, res, next) => {
 });
 
 /*--------------------------------------------------Radera Todo (Delete)-------------------------------------------------- */
-router.delete('/:id', async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
   let id = req.params.id; //Läs in id
 
   let todo = await Todo.findById(id);
 
   //Om todo hittades
-  if(todo) {
+  if (todo) {
     todo.deleteOne(); //Radera
     //Returnera meddelande
     res.send({
-      "status": "Lyckades",
-      "text": `Todo med Id ${id} raderades!`
+      status: "Lyckades",
+      text: `Todo med Id ${id} raderades!`,
     });
   }
 });
 
 /* --------------------Hantera felaktig Delete-anrop och Update (snyggare felmedd. ifall saknar id-parameter)-------------------- */
-router.delete('/', async (req, res, next) => {
+router.delete("/", async (req, res, next) => {
   res.status(400);
   res.send("Saknar parameter: :id");
 });
 
-router.put('/', async (req, res, next) => {
+router.put("/", async (req, res, next) => {
   res.status(400);
   res.send("Saknar parameter: :id");
 });
-
 
 module.exports = router;
